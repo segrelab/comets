8a7f01c0a20bc21f83e0aff01bad83e339ccd037
Jeremy M. Chacon <jeremymchacon@gmail.com>
2019-12-02 13:16:35 -0600

    removed an unnecessary multiplication from world2d.getmediarefreshamount

--- comets_simplified/src/edu/bu/segrelab/comets/World2D.java
+++ comets_simplified/src/edu/bu/segrelab/comets/World2D.java
@@ -876,9 +876,7 @@ public abstract class World2D implements CometsConstants, IWorld
 			if (refreshPoints[x][y] != null)
 				rpConc = refreshPoints[x][y].getMediaRefresh();
 			for (int i=0; i<numMedia; i++){
-				// JMC something weird is up, figure this out
-				// this doesn't get called during loading of layout--must be called just by gui
-				conc[i] = mediaRefresh[i] + rpConc[i] * cParams.getTimeStep();
+				conc[i] = mediaRefresh[i] + rpConc[i];
 			}
 
 			return conc;
