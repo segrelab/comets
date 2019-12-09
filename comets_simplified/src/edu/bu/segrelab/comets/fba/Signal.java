d1319f671dfe38713412edc3c19459fcdf0b2bfc
Jeremy Chacon <jeremymchacon@gmail.com>
2019-12-09 13:57:29 -0600

    added Signal class with bare bones

---
+++ comets_simplified/src/edu/bu/segrelab/comets/fba/Signal.java
@@ -0,0 +1,47 @@
+package edu.bu.segrelab.comets.fba;
+
+
+/**
+ * Signal is a simple class that holds information about how 
+ * a particular reaction's bounds are modified by the 
+ * local concentration of a metabolite acting as a signal.
+ * 
+ * FBAModel will likely hold a list of these, read from a 
+ * model file.  Functions will be called by FBACell.run(), 
+ * when bounds are being calculated.
+ * @author jeremy
+ *
+ */
+public class Signal {
+	private boolean lb, ub; // True if this signal affects these bounds
+	private int reaction; // the reaction number affected
+	private int exch_met;// the exchange met number of the affecting metabolite
+	//note:  above could easily be turned into a list, if multiple mets
+	//   can affect a reaction
+	//
+	//  main signaling function is Richard's, aka generalized logistic:
+	//
+	//   bound(met) = A + (K-A) / [(C + Q*exp(-B*met)^(1/v)]
+	private double A, K, C, Q, B, v;
+	
+	/**
+	 * creates a Signal object.  
+	 * @todo have these test for correct values (esp reactions and exch_met)
+	 * 
+	 */
+	public Signal(boolean lb, boolean ub, int reaction, int exch_met,
+			double A, double K, double C, double Q, double B, double v) {
+		this.lb = lb;
+		this.ub = ub;
+		this.reaction = reaction;
+		this.exch_met = exch_met;
+		this.A = A;
+		this.K = K;
+		this.C = C;
+		this.Q = Q;
+		this.B = B;
+		this.v = v;
+		
+	}
+
+}
